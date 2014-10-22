ea5f89333727aace6c636002c4ae94734434b41d
smartshinobu <bizan07@bizankomumakku07-no-MacBook-Air-2.local>
2014-10-22 21:36:35 +0900

    Initial Commit

---
+++ phppost2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  phppost2
+//
+//  Created by ビザンコムマック０７ on 2014/10/22.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ phppost2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  phppost2
+//
+//  Created by ビザンコムマック０７ on 2014/10/22.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
